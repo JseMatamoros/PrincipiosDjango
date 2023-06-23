3dd3c78f001f8bd470a86e1dda714cc9de723966
AryunDev <josematamoros916@gmail.com>
2023-06-22 22:39:41 -0400

    commit dia 9, sobreescritura y herencia de plantillas

--- primera/urls.py
+++ primera/urls.py
@@ -4,7 +4,7 @@ from .views import IndexPageView, obtener_fecha, menu_view
 
 urlpatterns = [
     path('', IndexPageView.as_view(), name ='index'),
-    path('fecha/<str:name>/', obtener_fecha, name ='fecha'),
+    path('fecha/<str:name>/<int:foto>/', obtener_fecha, name ='fecha'),
     path('menu/', menu_view, name="menu"),
 ]
 
--- primera/views.py
+++ primera/views.py
@@ -13,9 +13,15 @@ class IndexPageView(TemplateView):
 def index(request):
     return HttpResponse("HOLA ERICH")
 
-def obtener_fecha(request, name):
+def obtener_fecha(request, name, foto):
     fecha_actual = datetime.datetime.now()
-    context = {'fecha': fecha_actual, 'name': name}
+    # print 'fecha: {fecha_actual}' NO!
+    context = {
+        'fecha': fecha_actual, 
+        'name': name, 
+        'frutas': ['Manzana', 'Mango', 'Naranja'],
+        'foto': foto, 
+    }
     return render(request, 'fecha.html', context)
 
 def menu_view(request):
